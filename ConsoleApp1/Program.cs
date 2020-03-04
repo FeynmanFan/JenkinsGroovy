b8175e2e05308f1572e16ee14cea3e48a9e2118f
Chris Behrens <cbehrens@gmail.com>
2020-03-04 12:05:03 -0600

    Fixed problems, added dir script

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("I love Groovy!");
-
-            sdflkjsdflkjsdflkjsdlkfjsdlkfj
         }
     }
 }
