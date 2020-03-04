c6b03e110bc274a160351ed83da3729ae44dfaa6
Chris Behrens <cbehrens@gmail.com>
2020-03-03 20:37:09 -0600

    Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("I love Groovy!");
+
+            sdflkjsdflkjsdflkjsdlkfjsdlkfj
         }
     }
 }
